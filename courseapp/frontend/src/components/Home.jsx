6cbce7ceb45ab629714a5361a0fe9bfb28002715
rohansharma99 <rohansharmaji9411@gmail.com>
2025-12-08 08:10:45 +0530

    `Added validation for course ID in course.controller.js and course.route.js, and added validation for admin login credentials in admin.controller.js`

--- courseapp/frontend/src/components/Home.jsx
+++ courseapp/frontend/src/components/Home.jsx
@@ -215,13 +215,13 @@ function Home() {
               <h3 className="text-lg font-semibold md:mb-4">connects</h3>
               <ul className=" space-y-2 text-gray-400">
                 <li className="hover:text-white cursor-pointer duration-300">
-                  youtube- learn coding
+                  youtube-
                 </li>
                 <li className="hover:text-white cursor-pointer duration-300">
-                  telegram- learn coding
+                  telegram-
                 </li>
                 <li className="hover:text-white cursor-pointer duration-300">
-                  Github- learn coding
+                  Github-
                 </li>
               </ul>
             </div>
